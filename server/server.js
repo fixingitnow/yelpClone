75153cfe735a164dad64e3383eedb932fddb9043
pradeep <pradeep.uppula@webflow.com>
2023-10-22 18:15:06 -0700

    use parameterized query

--- server/server.js
+++ server/server.js
@@ -97,7 +97,7 @@ app.get("/api/v1/restaurants", async (req, res) => {
 // read one
 app.get("/api/v1/restaurants/:id", async (req, res) => {
   try{
-    const results = await db.query(`select * from restaurants where id=${req.params.id}`);
+    const results = await db.query(`select * from restaurants where id=$1`, [req.params.id]);
     res.status(200).json({
         status: "success",
         results: results.rows.length,
