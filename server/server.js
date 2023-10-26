93405e8272f9460137c042413799d8ca88ff5854
pradeep <pradeep.uppula@webflow.com>
2023-10-26 00:25:19 -0700

    update create api

--- server/server.js
+++ server/server.js
@@ -63,54 +63,57 @@ app.use(express.json());
 app.use(morgan("tiny"));
 
 // create
-app.post("/api/v1/restaurants", (req, res) => {
-  console.log(req.body);
-  // console.log(req)
-  res.status(201).json({
-    status: "success",
-    data: {
-      restaurant: "McDonalds",
-    },
-  });
+app.post("/api/v1/restaurants", async (req, res) => {
+  try {
+    const result = await db.query(
+      "INSERT INTO restaurants (name, price, on_sale) values ($1, $2, $3) returning *",
+      [req.body.name, req.body.price_range, req.body.on_sale]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // read all
 app.get("/api/v1/restaurants", async (req, res) => {
-    try{
-        const results = await db.query("select * from restaurants");
-        console.log(results);
-        res.status(200).json({
-            status: "success",
-            results: results.rows.length,
-            data: {
-            restaurants: results.rows,
-            },
-        });
-    } catch(err){
-        res.status(500).json({
-            status: "error",
-            message: err.message
-        })
-    }
+  try {
+    const results = await db.query("select * from restaurants");
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
 });
 
 // read one
 app.get("/api/v1/restaurants/:id", async (req, res) => {
-  try{
-    const results = await db.query(`select * from restaurants where id=$1`, [req.params.id]);
+  try {
+    const results = await db.query(`select * from restaurants where id=$1`, [
+      req.params.id,
+    ]);
     res.status(200).json({
-        status: "success",
-        results: results.rows.length,
-        data: {
+      status: "success",
+      results: results.rows.length,
+      data: {
         restaurants: results.rows,
-        },
+      },
     });
-} catch(err){
+  } catch (err) {
     res.status(500).json({
-        status: "error",
-        message: err.message
-    })
-}
+      status: "error",
+      message: err.message,
+    });
+  }
 });
 
 // update
