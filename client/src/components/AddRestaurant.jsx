5982845fa0385ef89acbf4d8ed0b8c2c4ffc5c56
pradeep <pradeep.uppula@webflow.com>
2023-10-28 11:57:06 -0700

    add(*): inital styling for mock components

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -2,20 +2,30 @@ import React from "react";
 
 const AddRestaurant = () => {
   return (
-    <form className="d-flex justify-content-center">
-      <label> Name</label>
-      <input></input>
-      <label>Location</label>
-      <input></input>
-      <label>Price range</label>
-      <select>
-        <option>$</option>
-        <option>$$</option>
-        <option>$$$</option>
-        <option value={"$$$$"}>$$$$</option>
-      </select>
-      <button>Add</button>
-    </form>
+    <div className="mb-4">
+      <form action="">
+        <div className="row">
+          <div className="col">
+            <input placeholder="Name"></input>
+          </div>
+          <div className="col">
+            <input placeholder="Location"></input>
+          </div>
+          <div className="col">
+            <label>Price range</label>
+            <select>
+              <option value={1}>$</option>
+              <option value={2}>$$</option>
+              <option value={3}>$$$</option>
+              <option value={4}>$$$$</option>
+            </select>
+          </div>
+          <div className="col">
+            <button className="btn btn-primary">Add</button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import AddRestaurant from "../components/AddRestaurant";
+import Restaurant from "../components/Restaurant";
 
 const Home = () => {
   return (
     <>
       <Header />
       <AddRestaurant />
+      <Restaurant />
     </>
   );
 };
